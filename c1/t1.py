37cd5e362ed658a79bd36882e00657ce8332b4fc
wlyjk12345 <wlyjk123456@163.com>
2019-09-03 23:02:06 +0800

    1

--- c1/t1.py
+++ c1/t1.py
@@ -1,11 +1,12 @@
-import cv2 as cv
+import cv2
 #读取图像，支持 bmp、jpg、png、tiff 等常用格式
-img = cv.imread("E:\DSC00006")
+img = cv2.imread("E:\DSC00006.jpg",cv2.IMREAD_COLOR)#彩照               # 读入黑白图片   ,cv2.IMREAD_GRAYSCALE)
 #创建窗口并显示图像
-cv.namedWindow("Imagea")
-cv.imshow("Image",img)
-cv.waitKey(0)
+cv2.namedWindow("Imagea")
+cv2.imshow("Imagea",img)
+k = cv2.waitKey(0)
 #释放窗口
-cv2.destroyAllWindows()
+if k==27:  #如果输入ESC退出
+   cv2.destroyAllWindows()
 
 
