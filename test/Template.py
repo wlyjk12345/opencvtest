755935b0b423eeeb4b52ccd410a2baea7e00b016
wlyjk12345 <wlyjk123456@163.com>
2019-09-17 20:57:09 +0800

    7

--- test/Template.py
+++ test/Template.py
@@ -1,22 +1,24 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-img = cv.imread("E:\p4.jpg",cv.IMREAD_COLOR)
+img = cv.imread("E:\p5.jpg",cv.IMREAD_COLOR)
 cv.namedWindow("result",cv.WINDOW_NORMAL)
-img = cv.edgePreservingFilter(img, sigma_s=100, sigma_r=0.4, flags=cv.RECURS_FILTER)  #保边滤波器
+#img = cv.edgePreservingFilter(img, sigma_s=100, sigma_r=0.4, flags=cv.RECURS_FILTER)  #保边滤波器
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-gray= cv.medianBlur(gray, 9)
+#gray= cv.medianBlur(gray, 9)
+
 result1 = cv.GaussianBlur(gray, (9, 9), 0)
 #dst = cv.pyrMeanShiftFiltering(img, 15, 30, termcrit=(cv.TERM_CRITERIA_MAX_ITER+cv.TERM_CRITERIA_EPS, 5, 1))
-
-'''# 顶帽操作
-se = cv.getStructuringElement(cv.MORPH_RECT, (3, 3), (-1, -1))
-binary = cv.morphologyEx(binary, cv.MORPH_TOPHAT, se)
-edges = cv.Canny((result1),1,10)
+ret, result1 = cv.threshold(result1, 127, 255, cv.THRESH_TOZERO)
+# 顶帽操作
+se = cv.getStructuringElement(cv.MORPH_RECT, (9, 9), (-1, -1))
+binary = cv.morphologyEx(result1, cv.MORPH_BLACKHAT, se)
+ret, th5 = cv.threshold(binary, 127, 255, cv.THRESH_TOZERO_INV)
+edges = cv.Canny((th5),1,10)
 '''
 dst = cv.Laplacian(result1, cv.CV_32F, ksize=3, delta=127) #
 img = cv.convertScaleAbs(dst)
-'''
+
 sharpen_op = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]], dtype=np.float32)
 sharpen_image = cv.filter2D(dst, cv.CV_32F, sharpen_op)
 sharpen_image = cv.convertScaleAbs(sharpen_image)
@@ -26,16 +28,16 @@ sharpen_image = cv.convertScaleAbs(sharpen_image)
 h, w = gray.shape[:2]
 result = np.zeros([h, w*3], dtype=gray.dtype)
 result[0:h,0:w] = result1
-result[0:h,w:2*w] = dst
-result[0:h,2*w:3*w] = img
+result[0:h,w:2*w] = th5
+result[0:h,2*w:3*w] = edges
 result = cv.resize(result, (w*3,h ))
 
 cv.imshow("result", result)
-ret, th1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
-ret, th2 = cv.threshold(img, 127, 255, cv.THRESH_BINARY_INV)
-ret, th3 = cv.threshold(img, 127, 255, cv.THRESH_TRUNC)
-ret, th4 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO)
-ret, th5 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO_INV)
+ret, th1 = cv.threshold(binary, 127, 255, cv.THRESH_BINARY)
+ret, th2 = cv.threshold(binary, 127, 255, cv.THRESH_BINARY_INV)
+ret, th3 = cv.threshold(binary, 127, 255, cv.THRESH_TRUNC)
+ret, th4 = cv.threshold(binary, 127, 255, cv.THRESH_TOZERO)
+ret, th5 = cv.threshold(binary, 127, 255, cv.THRESH_TOZERO_INV)
 titles = ['Original', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
 images = [img, th1, th2, th3, th4, th5]
 # 使用Matplotlib显示
