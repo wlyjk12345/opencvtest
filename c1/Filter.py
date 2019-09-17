c187556ee2facf4cd736380cd48e5c8b610c9a0a
wlyjk12345 <wlyjk123456@163.com>
2019-09-17 11:14:57 +0800

    7

--- c1/Filter.py
+++ c1/Filter.py
@@ -1,9 +1,9 @@
 import cv2 as cv
 import numpy as np
-
-src = cv.imread("E:\p1.png")
+import matplotlib.pyplot as plt
+img = cv.imread("E:\p1.png")
 cv.namedWindow("input", cv.WINDOW_AUTOSIZE)
-cv.imshow("input", src)
+cv.imshow("input", img)
 '''
 blur_op = np.ones([5, 5], dtype=np.float32)/25.
 shape_op = np.array([[0, -1, 0],
@@ -27,10 +27,27 @@ absX = cv.convertScaleAbs(x)
 absY = cv.convertScaleAbs(y) #转回uint8
 dst = cv.addWeighted(absX, 0.5, absY, 0.5, 0) #组合起来
 #dst = cv2.Sobel(src, ddepth图像的深度012, dx求导的阶数, dy求导的阶数[, dst[, ksize算子的大小[, scale[, delta[, borderType]]]]])
-'''
+
 dst = cv.Laplacian(src, cv.CV_32F, ksize=3, delta=127)#拉普拉斯算子
 dst = cv.convertScaleAbs(dst)
 
-cv.imshow('ss',dst)
+usm = cv.addWeighted(gray, 1.5, dst, -0.5, 0) #usm sharpen
+'''
+'''
+ret, th1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+ret, th2 = cv.threshold(img, 127, 255, cv.THRESH_BINARY_INV)
+ret, th3 = cv.threshold(img, 127, 255, cv.THRESH_TRUNC)
+ret, th4 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO)
+ret, th5 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO_INV)
+titles = ['Original', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+images = [img, th1, th2, th3, th4, th5]
+# 使用Matplotlib显示
+for i in range(6):
+    plt.subplot(2, 3, i + 1)
+    plt.imshow(images[i], 'gray')
+    plt.title(titles[i], fontsize=8)
+    plt.xticks([]), plt.yticks([])  # 隐藏坐标轴
+plt.show()
+,,,'''
 cv.waitKey(0)
 cv.destroyAllWindows()
--- test/Template.py
+++ test/Template.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 img = cv.imread("E:\p4.jpg",cv.IMREAD_COLOR)
 cv.namedWindow("result",cv.WINDOW_NORMAL)
 img = cv.edgePreservingFilter(img, sigma_s=100, sigma_r=0.4, flags=cv.RECURS_FILTER)  #保边滤波器
@@ -14,22 +15,36 @@ binary = cv.morphologyEx(binary, cv.MORPH_TOPHAT, se)
 edges = cv.Canny((result1),1,10)
 '''
 dst = cv.Laplacian(result1, cv.CV_32F, ksize=3, delta=127) #
-dst = cv.convertScaleAbs(dst)
-
+img = cv.convertScaleAbs(dst)
+'''
 sharpen_op = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]], dtype=np.float32)
 sharpen_image = cv.filter2D(dst, cv.CV_32F, sharpen_op)
 sharpen_image = cv.convertScaleAbs(sharpen_image)
+'''
+#edges = cv.Canny((dst),1,10)
 
-edges = cv.Canny((dst),1,10)
 h, w = gray.shape[:2]
 result = np.zeros([h, w*3], dtype=gray.dtype)
 result[0:h,0:w] = result1
 result[0:h,w:2*w] = dst
-result[0:h,2*w:3*w] =
-result = cv.resize(result, (w*2,h ))
+result[0:h,2*w:3*w] = img
+result = cv.resize(result, (w*3,h ))
 
 cv.imshow("result", result)
-
+ret, th1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+ret, th2 = cv.threshold(img, 127, 255, cv.THRESH_BINARY_INV)
+ret, th3 = cv.threshold(img, 127, 255, cv.THRESH_TRUNC)
+ret, th4 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO)
+ret, th5 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO_INV)
+titles = ['Original', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+images = [img, th1, th2, th3, th4, th5]
+# 使用Matplotlib显示
+for i in range(6):
+    plt.subplot(2, 3, i + 1)
+    plt.imshow(images[i], 'gray')
+    plt.title(titles[i], fontsize=8)
+    plt.xticks([]), plt.yticks([])  # 隐藏坐标轴
+plt.show()
 
 k = cv.waitKey(0)
 if k==27:
