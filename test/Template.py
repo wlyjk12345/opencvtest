9c10d3d23f37e6b79b26fe691e679c154443139f
wlyjk12345 <wlyjk123456@163.com>
2019-09-24 10:30:09 +0800

    8

--- test/Template.py
+++ test/Template.py
@@ -1,20 +1,30 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-img = cv.imread("E:\p5.jpg",cv.IMREAD_COLOR)
+img = cv.imread("E:\p8.jpg",cv.IMREAD_COLOR)
 cv.namedWindow("result",cv.WINDOW_NORMAL)
+
+shape_op = np.array([[0, -1, 0],
+                   [-1, 5, -1],
+                   [0, -1, 0]], np.float32)
+img = cv.filter2D(img, -1, shape_op)
 #img = cv.edgePreservingFilter(img, sigma_s=100, sigma_r=0.4, flags=cv.RECURS_FILTER)  #保边滤波器
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 #gray= cv.medianBlur(gray, 9)
 
-result1 = cv.GaussianBlur(gray, (9, 9), 0)
+#result1 = cv.GaussianBlur(gray, (9, 9), 0)
+binary =  gray
+result1 = gray
 #dst = cv.pyrMeanShiftFiltering(img, 15, 30, termcrit=(cv.TERM_CRITERIA_MAX_ITER+cv.TERM_CRITERIA_EPS, 5, 1))
-ret, result1 = cv.threshold(result1, 127, 255, cv.THRESH_TOZERO)
-# 顶帽操作
-se = cv.getStructuringElement(cv.MORPH_RECT, (9, 9), (-1, -1))
-binary = cv.morphologyEx(result1, cv.MORPH_BLACKHAT, se)
-ret, th5 = cv.threshold(binary, 127, 255, cv.THRESH_TOZERO_INV)
-edges = cv.Canny((th5),1,10)
+
+#se = cv.getStructuringElement(cv.MORPH_RECT, (5, 5), (-1, -1))# 顶帽操作
+#binary = cv.morphologyEx(result1, cv.MORPH_BLACKHAT, se)
+
+ret, th = cv.threshold(binary, 165, 255, cv.THRESH_BINARY)
+
+#t= cv.GaussianBlur(th, (9, 9), 0)
+t = cv.medianBlur(th, 9)
+edges = cv.Canny((t),1,10)
 '''
 dst = cv.Laplacian(result1, cv.CV_32F, ksize=3, delta=127) #
 img = cv.convertScaleAbs(dst)
@@ -27,13 +37,13 @@ sharpen_image = cv.convertScaleAbs(sharpen_image)
 
 h, w = gray.shape[:2]
 result = np.zeros([h, w*3], dtype=gray.dtype)
-result[0:h,0:w] = result1
-result[0:h,w:2*w] = th5
+result[0:h,0:w] = gray
+result[0:h,w:2*w] =   binary#result1
 result[0:h,2*w:3*w] = edges
 result = cv.resize(result, (w*3,h ))
 
 cv.imshow("result", result)
-ret, th1 = cv.threshold(binary, 127, 255, cv.THRESH_BINARY)
+ret, th1 = cv.threshold(binary, 170, 255, cv.THRESH_BINARY)
 ret, th2 = cv.threshold(binary, 127, 255, cv.THRESH_BINARY_INV)
 ret, th3 = cv.threshold(binary, 127, 255, cv.THRESH_TRUNC)
 ret, th4 = cv.threshold(binary, 127, 255, cv.THRESH_TOZERO)
